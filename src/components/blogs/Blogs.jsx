e363c180cfb10f08b758547c1a58186164a56edc
hibiscus06 <rudrikasingh1856@gmail.com>
2024-07-23 09:54:07 +0530

    change in blog

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
 import BlogCard from "./BlogCard";
-<<<<<<< HEAD
-=======
-import Footer from "../footer/Footer";
->>>>>>> 76792a7a92860790e1f7ff7e1cacc7461d355b6a
 
 const Blogs = () => {
   return (
     <>
       <Navbar />
-<<<<<<< HEAD
-=======
-      <div className="text-center ">
-        <p className="text-3xl font-serif font-bold text-orange-900">
-          Discover unique recipies and healthy tips and tricks!
-        </p>
-      </div>
->>>>>>> 76792a7a92860790e1f7ff7e1cacc7461d355b6a
       <div className="flex flex-row flex-wrap justify-evenly">
         <BlogCard />
         <BlogCard />
@@ -26,12 +14,8 @@ const Blogs = () => {
         <BlogCard />
         <BlogCard />
       </div>
-<<<<<<< HEAD
-    </div>
-=======
       <Footer />
     </>
->>>>>>> 76792a7a92860790e1f7ff7e1cacc7461d355b6a
   );
 };
 
